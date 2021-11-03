c35bdf1d8c52b983ea601408266bac50ae25f105
Ricardo Olalquiaga <66755477+Icksir@users.noreply.github.com>
2021-11-03 09:38:16 -0300

    Add files via upload

--- parserxml.py
+++ parserxml.py
@@ -4,6 +4,24 @@ import requests
 from xml.etree import ElementTree as ET
 r = requests.get("https://www.curriculumnacional.cl/estudiante/621/xml-article-239657.xml")
 root = ET.fromstring(r.content)
+<<<<<<< HEAD
+# iterate over root
+for child in root:
+    for cchild in child:
+        
+        
+        #print()
+
+        if child.attrib['bid'] == '9480':
+            print("TITULO: " + cchild.find("name").text) #TITULO
+
+            if cchild.find("binaries").find("binary").attrib['id'] == "textoescolar_descarga":
+                print("\tLINK: "+ cchild.find("binaries").find("binary").find("link").text)
+                print("\tLINK: "+ cchild.find("binaries").find("binary").find("thumb_link").text)
+        
+
+        #print(cchild.find("binaries").find("binary").attrib['id'])
+=======
 
 for child in root: # itera por cada hijo del xml
     for cchild in child: # itera por cada artículo
@@ -20,3 +38,4 @@ for child in root: # itera por cada hijo del xml
         # Si es mp3
         else: 
             print("|__ Miniatura: No tienen miniatura los mp3\n")
+>>>>>>> 71b9c0520f498858bdc9abd3a26d00067ff1ee2a
